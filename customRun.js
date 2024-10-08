652d007e52447a20e7a0aab4a2ca57b053a90dfe
githeimer <dulalhimesh@gmail.com>
2024-10-08 23:57:15 +0545

    minor change

--- customRun.js
+++ customRun.js
@@ -1,18 +1,19 @@
-import { exec } from 'child_process';
+import { exec } from "child_process";
 
 const runCommandInNewTerminal = (command, path) => {
-    exec(`start cmd.exe /K "cd ${path} && ${command}"`, (error, stdout, stderr) => {
-        if (error) {
-            console.error(`ERROR: ${error.message}`);
-            return;
-        }
-        console.log(`stdout: ${stdout}`);
-        console.error(`stderr: ${stderr}`);
-    });
+  exec(
+    `start cmd.exe /K "cd ${path} && ${command}"`,
+    (error, stdout, stderr) => {
+      if (error) {
+        console.error(`ERROR occured: ${error.message}`);
+        return;
+      }
+      console.log(`stdout: ${stdout}`);
+      console.error(`stderr: ${stderr}`);
+    }
+  );
 };
 
-
-
-//replace these with your command and file location 
-runCommandInNewTerminal('npm run dev', './client');
-runCommandInNewTerminal('npm start', './backend');
+//replace these with your command and file location
+runCommandInNewTerminal("npm run dev", "./client");
+runCommandInNewTerminal("npm start", "./backend");
